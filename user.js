8d66c486daf07ea066771a1cf741aab15bff6397
Owen <67721175+Owen000@users.noreply.github.com>
2020-10-09 12:55:17 +0000

    Add files via upload

---
+++ user.js
@@ -0,0 +1,49 @@
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("app.normandy.first_run", false);
+user_pref("app.normandy.migrationsApplied", 10);
+user_pref("app.normandy.startupExperimentPrefs.browser.migrate.showBookmarksToolbarAfterMigration", false);
+user_pref("app.normandy.startupExperimentPrefs.extensions.formautofill.creditCards.available", true);
+user_pref("app.normandy.startupExperimentPrefs.extensions.formautofill.creditCards.enabled", true);
+user_pref("app.normandy.startupExperimentPrefs.extensions.formautofill.creditCards.hideui", true);
+user_pref("app.normandy.startupExperimentPrefs.media.videocontrols.picture-in-picture.video-toggle.mode", -1);
+user_pref("app.normandy.startupExperimentPrefs.pdfjs.renderInteractiveForms", false);
+user_pref("app.normandy.startupExperimentPrefs.security.bad_cert_domain_error.url_fix_enabled", false);
+user_pref("app.normandy.startupExperimentPrefs.security.remote_settings.intermediates.downloads_per_poll", 100);
+user_pref("app.normandy.startupRolloutPrefs.doh-rollout.enabled", true);
+user_pref("app.normandy.startupRolloutPrefs.doh-rollout.provider-steering.enabled", true);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.download.panel.shown", true);
+user_pref("browser.laterrun.enabled", true);
+user_pref("distribution.archlinux.bookmarksProcessed", true);
+user_pref("distribution.iniFile.exists.value", true);
+user_pref("doh-rollout.balrog-migration-done", true);
+user_pref("doh-rollout.doneFirstRun", true);
+user_pref("dom.forms.autocomplete.formautofill", true);
+user_pref("extensions.formautofill.addresses.usage.hasEntry", false);
+user_pref("extensions.webcompat.enable_shims", true);
+user_pref("extensions.webcompat.perform_injections", true);
+user_pref("extensions.webcompat.perform_ua_overrides", true);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.screenshots@mozilla.org", true);
+user_pref("pdfjs.enabledCache.state", true);
+user_pref("privacy.sanitize.pending", "[]");
+user_pref("services.sync.engine.addresses.available", true);
+user_pref("services.sync.engine.creditcards.available", true);
+user_pref("signon.usage.hasEntry", false);
+user_pref("toolkit.telemetry.pioneer-new-studies-available", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false); // see [NOTE] above FF58+
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false); // [FF55+]
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false); // [FF55+]
+user_pref("toolkit.telemetry.updatePing.enabled", false); // [FF56+]
+user_pref("toolkit.telemetry.bhrPing.enabled", false); // [FF57+] Background Hang Reporter
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false); // [FF57+]
+user_pref("toolkit.telemetry.coverage.opt-out", true); // [HIDDEN PREF]
+user_pref("toolkit.coverage.opt-out", true); // [FF64+] [HIDDEN PREF]
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", "");
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+
